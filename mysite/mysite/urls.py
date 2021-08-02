2566906b1a12f1ca5fcc5c5eed992d0790743e97
chinmaymufasa <34931442+chinmaymufasa@users.noreply.github.com>
2021-08-02 01:09:19 -0700

    Going online

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,10 +1,14 @@
 
-from django.conf.urls import include
+from django.conf.urls import include, url
 from django.urls import path
 from django.contrib import admin
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('chat.urls')),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+]
 ]
 
